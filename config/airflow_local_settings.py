192e4c64dd4368854ce35f05ef847bc848d85c5c
caiocubeiro <caiocubeiroedu@gmail.com>
2024-11-15 23:16:48 +0000

    v1

---
+++ config/airflow_local_settings.py
@@ -0,0 +1,5 @@
+from airflow.www.utils import UIAlert
+
+DASHBOARD_UIALERTS = [
+    UIAlert("Welcome to a LocalExecuter Airflow with SSL build with docker-compose, check the repo in github.com/caiocubeiro/basic_airflow"),
+]
